50e0cfcbe5cde985447e1c812f6cd6cef71837bd
unknown <Admin@L-QBL-1706.quantanite.com>
2026-02-01 18:41:44 +0600

    authentication setup done

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,16 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 import nodemailer from "nodemailer";
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_NAME,
+    pass: process.env.APP_PASS,
+  },
+});
+
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
